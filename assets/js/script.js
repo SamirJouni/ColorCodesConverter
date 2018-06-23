e18a246ff8c570ce9c5e138cfb341021a29b1f65
F1r3byte <f1r3byte@yandex.com>
2018-06-23 23:33:53 +0300

    added some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,8 @@ document.addEventListener('DOMContentLoaded', function () {
 	"use strict";
 /* This is it the main script file for general scripts regarding the page */
 
+	var inputField = document.getElementById('colorCode');
+	var button = document.getElementById('convert');
+	var output = document.getElementById('output');
+
 });
