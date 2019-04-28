8de01ec109aeb8c698ad34589602d2330402fb02
F1r3byte <f1r3byte@yandex.com>
2019-04-28 23:11:20 +0200

    ran text formatter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function() {
 	"use strict";
 	/* This is it the main script file for general scripts regarding the page */
 
 	/* These are variables that will be needed later in the script */
-	var inputField = document.getElementById('colorCode');
-	var button = document.getElementById('convert');
-	var output = document.getElementById('output');
-	var body = document.getElementsByTagName('body')[0];
-	var inputCache = '';
+	var inputField = document.getElementById("colorCode");
+	var button = document.getElementById("convert");
+	var output = document.getElementById("output");
+	var body = document.getElementsByTagName("body")[0];
+	var inputCache = "";
 
 	/* These are event listeners that call the getInput function when the user wants to submit the input */
-	inputField.addEventListener('keypress', getInput);
-	button.addEventListener('click', getInput);
+	inputField.addEventListener("keypress", getInput);
+	button.addEventListener("click", getInput);
 
 	/* This function checks if the user has input something, checks if the input is being submitted, then
 	caches the input and calls the function that will output the result of the conversion */
 	function getInput() {
 		if (event.keyCode === 13 || event.button === 0) {
 			if (inputValueSpecified()) {
-
 				inputCache = inputField.value;
 
 				setBackgroundToColor();
@@ -30,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 	/* Checks if the user has actually typed anything in the input field before submitting */
 	function inputValueSpecified() {
-		if (inputField.value !== '') {
+		if (inputField.value !== "") {
 			return true;
 		}
 
@@ -40,12 +39,12 @@ document.addEventListener('DOMContentLoaded', function () {
 	/* This function calls a function that converts the input and then creates and appends the
 	output element */
 	function createOutputElement() {
-		var outputElement = document.createElement('span');
+		var outputElement = document.createElement("span");
 
-		outputElement.textContent = 'The Color is: ' + convertColorInput();
+		outputElement.textContent = "The Color is: " + convertColorInput();
 		output.appendChild(outputElement);
 
-		inputField.value = '';
+		inputField.value = "";
 	}
 
 	/* This function cleans up previous outputs to make sure older outputs don't stick around */
@@ -59,25 +58,26 @@ document.addEventListener('DOMContentLoaded', function () {
 	with auto detection for the inserted format. It also sets the input cache to always be in hex
 	format for the background setting function. */
 	function convertColorInput() {
-		if (inputCache[0] === '#') {
-
+		if (inputCache[0] === "#") {
 			return convertColor(inputCache);
 		} else {
-
 			inputCache = convertColor(inputCache);
 			return inputCache;
 		}
-
 	}
 
 	/* This function changes the background to a gradient of the color being input and a complimentary
 	color */
 	function setBackgroundToColor() {
-		body.style.backgroundImage = 'linear-gradient(' +
-			'90deg' + ', ' + inputCache + ', ' + shiftColor() + ')';
+		body.style.backgroundImage =
+			"linear-gradient(" +
+			"90deg" +
+			", " +
+			inputCache +
+			", " +
+			shiftColor() +
+			")";
 	}
 
-	function shiftColor() {
-
-	}
+	function shiftColor() {}
 });
